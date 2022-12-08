1a3f3bb3e475debf9a6f77a2f6a5126ac2672b63
Adryan Zielonka <adryanzielonka1@gmail.com>
2022-12-08 19:42:07 -0300

    Criada estrutura com erro.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,15 @@ namespace moduloGit
             * Crie um programa que receba dois valores e calcule a soma desses valores.
             * Ao final exiba o resultado no console.
             */
+
+            #region
+            Console.WriteLine("Digite 1 para INICIAR o programa ou 0 para SAIR.");
+            int opcao = int.Parse(Console.ReadLine());
+            while (opcao != 0)
+            {
+
+            }
+            #endregion
         }
     }
 }
