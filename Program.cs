71a2877bcf3e6f11a0e1a31ba975d9a50810ef13
Adryan Zielonka <adryanzielonka1@gmail.com>
2022-12-09 19:25:44 -0300

    Remoção da variável

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace moduloGit
             * Ao final exiba o resultado no console.
             */
 
-            #region
+            #region exercicio
             Console.WriteLine("Digite 1 para INICIAR o programa ou 0 para SAIR.");
             int opcao = int.Parse(Console.ReadLine());
             while (opcao != 0)
@@ -21,8 +21,7 @@ namespace moduloGit
                 Console.WriteLine("Informe 2 números para somá-los.");
                 a = int.Parse(Console.ReadLine());
                 b = int.Parse(Console.ReadLine());
-                int resultado = Soma(a, b);
-                Console.WriteLine("A soma é: " + resultado);
+                Console.WriteLine("A soma é: " + Soma(a,b));
                 Console.WriteLine("Se deseja sair, digite 0, senão 1.");
                 opcao = int.Parse(Console.ReadLine());
             }
